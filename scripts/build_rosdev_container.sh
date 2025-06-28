d3c1183452c26285b2c17c3fa14f1b1a35831245
Sahil Kale <sahil.kale1@gmail.com>
2025-06-27 23:53:33 -0700

    Add in inital docker setup

---
+++ scripts/build_rosdev_container.sh
@@ -0,0 +1,1 @@
+docker build -t ros-dev-custom ./docker
---
+++ scripts/startup_docker_script.sh
@@ -0,0 +1,9 @@
+xhost +local:docker  # Only if using GUI on Pi desktop
+
+docker run -it \
+  --name ros-dev \
+  --network host \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v ~/Desktop/carpi:/carpi \
+  ros-dev-custom
